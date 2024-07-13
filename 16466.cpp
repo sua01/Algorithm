db8a711f15d5c8b7039f19cf78a6843e7713e79f
sua01 <sua0211@naver.com>
2024-07-13 23:48:06 +0900

    2-2

--- 16466.cpp
+++ 16466.cpp
@@ -1,33 +1,29 @@
 //백준 16466 - 콘서트
-#include <iostream>
+#include <cstdio>
 #include <algorithm>
 using namespace std;
 
 int main(){
   int n;
-  cin >> n; // 1차 티켓팅에서 팔린 티켓 수
+  scanf("%d", &n); // 1차 티켓팅에서 팔린 티켓 수
   
   // 1차 티켓팅에서 팔린 티켓 번호 입력받기
   int* num = new int[n];
   for(int i = 0; i < n; i++){
-    cin >> num[i];
+    scanf("%d", &num[i]);
   }
 
   sort(num, num + n); //오름차순 정렬
 
   // 2차 티켓팅
-  int min = 1;
+  int val = 1;
   for(int i = 0; i < n; i++){
-    if(min == num[i]){
-      min++;
-      
-    }
-    else{
-      min++;
-    }
+    if (val != num[i]) break; // 제일 작은 숫자
+
+    val++;
   }
 
-  cout << min++;
+  printf("%d", val);
 
   delete[] num;
   return 0;
--- 26145.cpp
+++ 26145.cpp
@@ -7,9 +7,23 @@ int main(){
   int n, m;
   scanf("%d %d", &n, &m); // 출제자, 검수자 수 입력
 
-  int* money = new int[n];  // 출제가 받은 출제비 입력
+  int* money = new int[n+m];  // 출제가 받은 출제비 입력
   for(int i = 0; i < n; i++)
     scanf("%d", &money[i]);
+
+  int share, sum;  // 나눠주기로 한 돈
+  for(int i = 0; i < n; i++){
+    sum = 0;
+    for(int j = 0; j < n+m; j++){
+      scanf("%d", &share);
+      money[j] += share;
+      sum += share;
+    }
+    money[i] -= sum;
+  }
+  
+  for(int i = 0; i < n+m; i++)
+    printf("%d ", money[i]);
 
   delete[] money;
   return 0;
